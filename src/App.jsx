1d10f537ec0f86e9511764adcc851fb399e9c85d
geo <onion0208@gmail.com>
2025-04-14 17:27:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import Weathercard from "./component/Weathercard";
+import Spinner from "./component/Spinner";
+
+const App = () => {
+  // input存放使用者輸入的城市名稱
+  const [city, setCity] = useState("");
+  // weather等api抓完資料後存放天氣資訊
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // 按下按鈕後，呼叫handleSearch函式
+  // 傳入使用者輸入的城市名稱，並將天氣資訊存到weather中
+  // 用async await加上try, catch來等待api回傳資料
+  const handleSearch = async (city) => {
+    try {
+      setLoading(true);
+      const apiKey = "70a4fe505eadfa475abc6484b25c7bfd";
+      // 注意要加上encodeURIComponent，不然會出現亂碼，才可以用中文查
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city
+      )}&appid=${apiKey}&units=metric&lang=zh_tw`;
+      setCity("");
+      const response = await fetch(url);
+      // response.json()會回傳一個promise，所以要用await等待api回傳資料
+      const data = await response.json();
+      console.log(data);
+      setWeather(data);
+      setError(false);
+      // 如果api回傳的資料中cod為404，代表查詢失敗，因為這api不會直接傳error，而是會傳一個cod為404的物件
+      if (data.cod === "404") {
+        setError(true);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error("查詢失敗", error);
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="bg-[#4A7DFF] h-screen flex-center">
+      {/* 天氣查詢 */}
+      <div className="flex-col-center mx-3 ">
+        <h1 className="text-4xl text-white font-bold py-1">城市天氣查詢</h1>
+        <p className="text-white/80 font-medium py-1">
+          輸入城市名稱，查詢即時天氣狀況
+        </p>
+        {/* 搜尋列 */}
+        <div className="flex my-5 shadow-xl">
+          <input
+            className="input"
+            type="text"
+            placeholder="請輸入城市名稱"
+            // 使用者輸入的城市名稱存到city中，並清空input框
+            // 注意onChange事件要傳入e，e.target.value才是使用者輸入的城市名稱
+            onChange={(e) => setCity(e.target.value)}
+            // 記得要加value，不然不會清空input框，確保 input 的值始終與 React state ( city ) 保持同步
+            value={city}
+          />
+          {/* 按下按鈕後，呼叫handleSearch函式，onClick裡面要傳一個箭頭函式，傳入使用者輸入的城市名稱 */}
+          <button className="btn" onClick={() => handleSearch(city)}>
+            查詢
+          </button>
+        </div>
+        {/* 天氣卡片 */}
+        {/* 傳整個物件到wethercard，也可以先傳需要的特定屬性 */}
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <p className="text-gray-700 font-light">
+            查詢失敗，請試試看其他城市或使用英文查詢。
+          </p>
+        ) : (
+          <Weathercard data={weather} />
+        )}
+        <footer className="text-white/80 text-[10px] font-medium px-5 py-3 bg-blue-600 rounded-full mt-6">
+          API資料來源：OpenWeatherMap <br />
+          Developer：GeorgeZhou 周致祥 ｜ IconDesigner：
+          <a
+            href="https://www.figma.com/community/file/1126777451931792118"
+            target="_blank"
+            className="underline hover:text-white"
+          >
+            BilalArve
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Weathercard.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Weathercard = ({ data }) => {
+  const { name, main, weather, wind, clouds } = data || {};
+  const { temp } = main || {};
+  // 注意要加上weather?.[0]，因為weather是一個陣列.
+  // ?.[0] ：安全地訪問陣列的第一個元素，如果 weather 是 undefined 或 null ，會返回 undefined 而不是拋出錯誤
+  const { description } = weather?.[0] || {};
+  const { speed } = wind || {};
+  const { all } = clouds || {};
+
+  // 根據雲量來判斷天氣狀況，JSX不能直接使用if else，把邏輯抽出來比較好閱讀
+  const getWeatherIcon = (cloudCover) => {
+    if (cloudCover < 20) return "/sunny.svg";
+    if (cloudCover < 50) return "/lesscloud.svg";
+    return "/cloud.svg";
+  };
+
+  return (
+    // 注意要加上data?.length > 0，不然會出現錯誤，因為data是一個物件，所以要加上length > 0
+    Object.keys(data || {}).length > 0 ? (
+      <div className="bg-[#c0e5ff] w-[390px] h-[350px] rounded-2xl shadow-xl flex flex-col">
+        <h2 className="text-5xl text-gradient font-extrabold  m-3">{name}</h2>
+        <div className="flex flex-center">
+          <img
+            src={getWeatherIcon(all)}
+            alt="cloud"
+            className="size-50 -ml-11"
+          />
+          <div>
+            <p className="text-7xl text-gradient font-extrabold">
+              {Math.round(temp)}℃
+            </p>
+            <p className="text-primary font-medium my-3">{description}</p>
+          </div>
+        </div>
+        <div className="flex-center gap-25 ">
+          <div className="flex flex-col items-center">
+            <img
+              src="/airflow.svg"
+              alt="airflow"
+              className="w-[24px] h-[24px]"
+            />
+            <p className="text-primary font-light my-2">
+              {parseInt(speed) * 3.6} km/h
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <img src="/rain.svg" alt="rain" className="w-[24px] h-[24px]" />
+            <p className="text-primary font-light my-2">{all}</p>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <></>
+    )
+  );
+};
+
+export default Weathercard;
